6f744883168214fdb4d57aa27d16f311788757fd
Lincoln <lincolnanc@gmail.com>
2021-11-12 11:00:03 -0300

    Html e css prontos

---
+++ CarteiraDeInvestimentosWeb/wwwroot/js/script.js
@@ -0,0 +1,15 @@
+﻿import { mdbRangeInput } from 'mdbvue';
+export default {
+    name: 'SelectPage',
+    components: {
+        mdbRangeInput
+    },
+    data() {
+        return {
+            range1Value: 25,
+            range2Value: 40,
+            range3Value: 0,
+            range4Value: 50
+        }
+    }
+}
